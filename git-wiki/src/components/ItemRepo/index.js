db341e28cec434af5c34934502c2a534a463d375
luiz-ricardo-dev <playsonbr@hotmail.com>
2024-10-31 19:24:19 -0300

    Sistema GitWiki criado com React.

---
+++ git-wiki/src/components/ItemRepo/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {ItemContainer} from './styles';
+
+ function ItemRepo({repo}) {
+  return (
+    <ItemContainer>
+        <h3>{repo.name}</h3>
+        <p>{repo.full_name}</p>
+        <a href={repo.html_url} target='_blank'>Ver repositório</a> <br />
+        <a href='#' className='remover'>Remover</a>
+        <hr/>
+
+    </ItemContainer>
+    
+  )
+}
+
+export default ItemRepo;
